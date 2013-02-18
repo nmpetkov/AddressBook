79cc5035c6dc5f1663f221c03366503899c46d1f
N. Petkov <nmpetkov@gmail.com>
2013-02-19 00:10:52 +0200

    contenttype

---
+++ contenttypesapi/address.php
@@ -0,0 +1,57 @@
+<?php
+class AddressBook_contenttypesapi_addressPlugin extends contentTypeBase
+{
+    var $addressid;
+
+    function getModule() { return 'AddressBook'; }
+    function getName() { return 'address'; }
+    function getTitle()
+    {
+        $dom = ZLanguage::getModuleDomain('AddressBook');
+        return __('AddressBook contact', $dom);
+    }
+    function getDescription()
+    {
+        $dom = ZLanguage::getModuleDomain('AddressBook');
+        return __('Display a single address.', $dom);
+    }
+    function isTranslatable() { return false; }
+
+    function loadData($data)
+    {
+        $this->addressid = $data['addressid'];
+    }
+
+    function display()
+    {
+        $address = ModUtil::func('AddressBook', 'user', 'simpledisplay', array('id' => (int) $this->addressid));
+        return $address;
+    }
+
+    function displayEditing()
+    {
+        $dom = ZLanguage::getModuleDomain('AddressBook');
+        if (!empty($this->addressid))
+        {
+            return ModUtil::func('AddressBook', 'user', 'simpledisplay', array('id' => (int) $this->addressid));
+        }
+        return __('No address selected', $dom);
+    }
+
+    function getDefaultData()
+    {
+        return array('addressid' => '');
+    }
+
+
+    function startEditing(&$render)
+    {
+        array_push($render->plugins_dir, 'modules/AddressBook/pntemplates/pnform');
+    }
+}
+
+
+function AddressBook_contenttypesapi_address($args)
+{
+    return new AddressBook_contenttypesapi_addressPlugin();
+}
---
+++ templates/contenttype/address_edit.tpl
@@ -0,0 +1,5 @@
+{* $Id: address_edit.tpl 70 2010-04-01 14:46:28Z herr.vorragend $ *}
+<div class="z-formrow">
+    {formlabel for="addressid" __text='Please enter the address id'}
+    {formtextinput id='addressid' maxLength='2' width="30px" group='data'}
+</div>
